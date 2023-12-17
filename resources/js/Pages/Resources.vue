721fad234a8e7dc92381cf3b3570e220520477c6
Jeison-ortiz <ortizjeison4@gmail.com>
2023-12-16 23:09:41 -0500

    feat: Add the vote column and the icon to vote.

--- resources/js/Pages/Resources.vue
+++ resources/js/Pages/Resources.vue
@@ -26,23 +26,34 @@ watch(search, (value) => {
     axios.get("/api/resources?search=" + value + "&category="+ filteredCategory.value).then((response)=>{
         filteredResources.value = response.data;
 
-});
+    });
 })
 
 watch(filteredCategory, (value) => {
     axios.get("/api/resources?category=" + value + "&search=" + search.value).then((response)=>{
         filteredResources.value = response.data;
 
-});
-
+    });
 })
 
 onMounted(()=>{
     console.log("Recursos cargados!", props.resources);
     filteredResources.value = props.resources;
+})
+
+function vote(resourceId){
+    axios.get("/api/vote/" + resourceId).then((response)=>{
+
+        filteredResources.value = filteredResources.value.map((resource)=>{
+            if(resource.id===resourceId){
+                return response.data
+            }
+            return resource
+        });
+
+    });
 }
 
-)
 </script>
 
 <template>
@@ -93,7 +104,11 @@ onMounted(()=>{
             <div>
                 <select v-model="filteredCategory">
                     <option value="">Todas las categorias</option>
-                    <option v-for="category in categories" :key="category.id" :value="category.id">
+                    <option
+                        v-for="category in categories"
+                        :key="category.id"
+                        :value="category.id"
+                    >
                           {{ category.name }}
                     </option>
                 </select>
@@ -102,14 +117,47 @@ onMounted(()=>{
                 <table class="w-full text-sm text-left text-gray-500">
                     <thead class="text-lg text-gray-700 uppercase bg-gray-500">
                         <tr>
-                            <th scope="col" class="p-4">Recurso</th>
+                            <th scope="col" class="p-4">Votos</th>
+                            <th scope="col" class="p-4">Recursos</th>
                             <th scope="col">Link</th>
                             <th scope="col">Categoria</th>
                         </tr>
                     </thead>
                     <tbody class="bg-white">
                         <tr v-for="resource in filteredResources" :key="resources.id">
-                            <th scope="row" class="p-4">{{ resource.title }}</th>
+                            <th scope="row" class="p-4" text="left">
+                                <div class="flex">
+                                    <span>
+                                        {{ resource.votes.length }}
+                                    </span>
+                                    <button @click="vote(resource.id)">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5"
+                                            stroke="currentColor"
+                                            class="w-6 h-6 text-red-500">
+                                            <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                                        </svg>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5"
+                                            stroke="currentColor"
+                                            class="w-6 h-6">
+                                            <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                                        </svg>
+                                    </button>
+                                </div>
+
+                            </th>
+                            <th scope="row" class="p-4" text="left">{{ resource.title }}</th>
                             <th scope="row" class="p-4">
                                 <a target="_blank" :href="resource.link">Ver recurso</a>
                             </th>
